924316a91f9a1a195c18d4886b1bb5bb867cf4a5
Bart van Kuik <bart@dutchvirtual.nl>
2016-09-06 14:54:06 +0200

    First version

--- KeyboardAutolayoutTest/ViewController.swift
+++ KeyboardAutolayoutTest/ViewController.swift
@@ -10,16 +10,65 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var textFieldTopConstraint: NSLayoutConstraint!
+    @IBOutlet var textFieldTopLayoutGuideConstraint: NSLayoutConstraint!
+    @IBOutlet weak var textField: UITextField!
+
+    var textFieldTopLayoutGuideConstraintConstant: CGFloat?
+    
+    func keyboardWillShow(notification: NSNotification) {
+        print("keyboardWillShow")
+        self.textFieldTopConstraint.active = false
+        self.textFieldTopLayoutGuideConstraint.active = true
+        self.textFieldTopLayoutGuideConstraint.constant = 0
+        UIView.animateWithDuration(1.0, animations: {
+            self.view.layoutIfNeeded()
+            }) { (finished) in
+        }
+    }
+    
+    func keyboardDidShow(notification: NSNotification) {
+        print("keyboardDidShow")
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        print("keyboardWillHide")
+        if let constant = self.textFieldTopLayoutGuideConstraintConstant {
+            self.textFieldTopLayoutGuideConstraint.constant = constant
+        }
+        UIView.animateWithDuration(1.0, animations: {
+            self.view.layoutIfNeeded()
+
+            }) { (finished) in
+                self.textFieldTopLayoutGuideConstraint.active = false
+                self.textFieldTopConstraint.active = true
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(self.keyboardDidShow(_:)),
+                                                         name: UIKeyboardDidShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(self.keyboardWillShow(_:)),
+                                                         name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(self.keyboardWillHide(_:)),
+                                                         name: UIKeyboardWillHideNotification, object: nil)
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        if let _ = textFieldTopLayoutGuideConstraintConstant {
+            return
+        } else {
+            textFieldTopLayoutGuideConstraintConstant = textField.frame.origin.y
+            print("textFieldTopLayoutGuideConstraintConstant=\(textFieldTopLayoutGuideConstraintConstant)")
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    deinit {
+        NSNotificationCenter.defaultCenter().removeObserver(self)
     }
-
-
 }
 
